0ee633e2bf708fc03cd105530d7d67eb9a9c48a4
Bruno Afonso <bno.afonso@hotmail.com>
2020-05-06 01:41:24 -0300

    listagem e cadastro de planos

---
+++ resources/views/admin/pages/plans/index.blade.php
@@ -0,0 +1,43 @@
+
+@extends('adminlte::page')
+
+@section('title', 'Planos')
+
+@section('content_header')
+    <h1>Planos <a href="{{route('plans.create')}}" class="btn btn-dark">ADD</a></h1>
+@stop
+
+@section('content')
+    <div class="card-header">
+
+        <div class="card-body">
+            <table class="table table-condensed">
+                <thead> 
+                    <tr>
+                        <th>Nome</th>
+                        <th>Preço</th>
+                        <th>Ações</th>
+                    </tr>
+                <thead>  
+                <tbody>
+                    @foreach($plans as $plan)
+                        <tr>
+                            <td>
+                                {{$plan->name}}
+                            </td>
+                            <td>
+                                {{$plan->price}}
+                            </td>
+                            <td>
+                                <a href=""class="btn btn-warning">Ver</a>
+                            </td>
+                        </tr>    
+                    @endforeach    
+                <tbody>  
+            </table>    
+        </div>
+        <div class="card-footer">
+            {{ $plans->links() }}
+        </div>
+    </div>    
+@stop
